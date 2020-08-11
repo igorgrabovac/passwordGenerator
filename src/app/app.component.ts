a0bc3521c4956311d98aa05ee8d7c9e8f666183f
Igorcar <grabovac.igor@gmail.com>
2020-08-11 15:38:24 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'password';
+    letters = false;
+    numbers = false;
+    symbols = false;
+    length = 0;
+    password = "";
+
+    onChangeLetters(){
+      this.letters = !this.letters;
+    }
+    onChangeNumbers(){
+      this.numbers = !this.numbers;
+    }
+    onChangeSymbols(){
+      this.symbols = !this.symbols;
+    }
+
+    passwordLength = (e)=>{
+     let value = parseInt(e);
+     if(!isNaN(value))
+     this.length = value;
+    }
+
+   
+    showPassword() {
+      const allLetters: string = "zxcvbnmasdfghjklqwertyuiop";
+      const allNumbers: string = "1234567890";
+      const allSymbols: string = "!@#$%^&*()-_+=";
+  
+      let passwordPool = "";
+      if (this.letters){
+        passwordPool += allLetters;
+      }
+      if (this.numbers){
+        passwordPool += allNumbers;
+      }
+      if (this.symbols){
+        passwordPool += allSymbols;
+      }
+        
+      let generatedPassword = "";
+      for (let i = 0; i < this.length; i++) {
+        const index = Math.floor(Math.random() * passwordPool.length);
+        generatedPassword += passwordPool[index];
+        
+      }
+      this.password = generatedPassword;
+    }
+
 }
